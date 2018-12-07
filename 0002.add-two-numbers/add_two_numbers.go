7a389852825e2224d4f43ab09c13bed154f58911
Maosen Chen <maosen1993@gmail.com>
2018-12-06 22:41:48 -0500

    Add two numbers (#2)
    * Add two numbers

---
+++ 0002.add-two-numbers/add_two_numbers.go
@@ -0,0 +1,31 @@
+package problem2
+
+// ListNode for singly-linked list
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	currNode := dummyHead
+	carry := 0
+	a, b := l1, l2
+
+	for a != nil || b != nil || carry > 0 {
+		val := carry
+		if a != nil {
+			val += a.Val
+			a = a.Next
+		}
+		if b != nil {
+			val += b.Val
+			b = b.Next
+		}
+		currNode.Next = &ListNode{Val: val % 10}
+		currNode = currNode.Next
+		carry = val / 10
+	}
+
+	return dummyHead.Next
+}
