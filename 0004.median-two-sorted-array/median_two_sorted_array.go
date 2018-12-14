931b722a44f678689db12f98f7b31125756bac1f
maosenchen6ix <mchen@solution-tek.com>
2018-12-14 12:56:30 -0500

    Working solution

--- 0004.median-two-sorted-array/median_two_sorted_array.go
+++ 0004.median-two-sorted-array/median_two_sorted_array.go
@@ -12,84 +12,12 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		return findMedianSingleArray(nums1)
 	}
 	if l1 == 1 && l2 == 1 {
-		return float64(nums1[0]+nums2[2]) / 2
+		return float64(nums1[0]+nums2[0]) / 2
 	}
-	// isEven := (l1+l2)%2 == 0
-	if l1 == 1 {
-		if nums1[0] <= nums2[l1/2-1] {
-			return float64(nums2[l1/2-1])
-		}
-		if nums1[0] <= nums2[l1/2] {
-			return float64(nums1[0])
-		}
-		return float64(nums2[l1/2])
+	if l1 <= l2 {
+		return findMedian(nums1, nums2, l1, l2, 0, l1)
 	}
-	if l2 == 1 {
-		if nums2[0] <= nums1[l1/2-1] {
-			return float64(nums1[l1/2-1])
-		}
-		if nums2[0] <= nums1[l1/2] {
-			return float64(nums2[0])
-		}
-		return float64(nums1[l1/2])
-	}
-	if nums1[l1-1] <= nums2[0] {
-		if l1 < l2 {
-			return float64(nums2[(l2+l1)/2-l1])
-		}
-		return float64(nums1[(l1+l2)/2])
-	}
-	if nums2[l2-1] <= nums1[0] {
-		if l2 < l1 {
-			return float64(nums1[(l2+l1)/2-l2])
-		}
-		return float64(nums2[(l1+l2)/2])
-	}
-
-	k1 := findMedian(nums1, nums2, l1, l2, 0, l1-1)
-	if k1 > -1 {
-		return float64(nums1[k1])
-	}
-	// if k1 >= 0 {
-	// 	m1 := nums1[k1]
-	// 	if isEven {
-	// 		var m2 int
-	// 		if k1 < l1-1 {
-	// 			a := nums1[k1+1]
-	// 			b := nums2[(l1+l2)/2-k1]
-	// 			if a <= b {
-	// 				m2 = a
-	// 			} else {
-	// 				m2 = b
-	// 			}
-	// 		} else {
-	// 			b := nums2[(l1+l2)/2-k1]
-	// 			m2 = b
-	// 		}
-	// 		return float64(m1+m2) / 2
-	// 	}
-	// 	return float64(m1)
-	// }
-	k2 := findMedian(nums2, nums1, l2, l1, 0, l2-1)
-
-	// m1 := nums2[k2]
-	// if isEven {
-	// 	var m2 int
-	// 	if k1 < l1-1 {
-	// 		a := nums2[k2+1]
-	// 		b := nums1[(l1+l2)/2-k2]
-	// 		if a <= b {
-	// 			m2 = a
-	// 		} else {
-	// 			m2 = b
-	// 		}
-	// 	} else {
-	// 		b := nums1[(l1+l2)/2-k2]
-	// 		m2 = b
-	// 	}
-	// 	return float64(m1+m2) / 2
-	// }
-	return float64(nums2[k2])
+	return findMedian(nums2, nums1, l2, l1, 0, l2)
 }
 
 // Helper function to find median of a single sorted array
@@ -102,33 +30,79 @@ func findMedianSingleArray(nums []int) float64 {
 	return m
 }
 
-func findMedian(nums1 []int, nums2 []int, l1 int, l2 int, low int, heigh int) int {
-	if low > heigh {
-		return -1
+// find the partition i, j where nums1[i-1] <= nums2[j] && nums2[j-1] <= num1[i]
+// find the median assuming l1 <= l2
+func findMedian(nums1 []int, nums2 []int, l1 int, l2 int, low int, heigh int) float64 {
+	var maxFirstHalf, minSecondHalf float64
+	isEven := (l1+l2)%2 == 0
+	if l1 == 1 {
+		if nums1[0] <= nums2[l2/2] {
+			maxFirstHalf = float64(nums1[0])
+			if nums2[l2/2-1] > nums1[0] {
+				maxFirstHalf = float64(nums2[l2/2-1])
+			}
+			if !isEven {
+				return maxFirstHalf
+			}
+			return (maxFirstHalf + float64(nums2[l2/2])) / 2
+		}
+		maxFirstHalf = float64(nums2[l2/2])
+		if !isEven {
+			return maxFirstHalf
+		}
+		minSecondHalf = float64(nums1[0])
+		if nums1[0] >= nums2[l2/2+1] {
+			minSecondHalf = float64(nums2[l2/2+1])
+		}
+		return (maxFirstHalf + minSecondHalf) / 2
 	}
-	k := (low + heigh) / 2
-	if k <= (l1-l2)/2 {
-		return findMedian(nums1, nums2, l1, l2, k+1, heigh)
+
+	if nums1[l1-1] <= nums2[0] {
+		if l1 == l2 {
+			return float64(nums1[l1-1]+nums2[0]) / 2
+		}
+		maxFirstHalf = float64(nums2[(l2-l1+1)/2-1])
+		if !isEven {
+			return maxFirstHalf
+		}
+		minSecondHalf = float64(nums2[(l2-l1+1)/2])
+		return (maxFirstHalf + minSecondHalf) / 2
 	}
-	if k >= (l1+l2)/2 {
-		return findMedian(nums1, nums2, l1, l2, low, k-1)
+
+	if nums1[0] >= nums2[l2-1] {
+		if l1 == l2 {
+			return float64(nums1[0]+nums2[l2-1]) / 2
+		}
+		maxFirstHalf = float64(nums2[(l2+l1+1)/2-1])
+		if !isEven {
+			return maxFirstHalf
+		}
+		minSecondHalf = float64(nums2[(l2+l1+1)/2])
+		return (maxFirstHalf + minSecondHalf) / 2
 	}
-	if nums1[k] >= nums2[(l1+l2)/2-k-1] && nums1[k] <= nums2[(l1+l2)/2-k] {
-		return k
+
+	// partition nums1 and nums2
+	i := (low + heigh) / 2
+	j := (l1+l2+1)/2 - i
+	if nums1[i-1] > nums2[j] {
+		return findMedian(nums1, nums2, l1, l2, low, i)
 	}
-	if nums1[k] > nums2[(l1+l2)/2-k] {
-		return findMedian(nums1, nums2, l1, l2, low, k-1)
+	if nums2[j-1] > nums1[i] {
+		return findMedian(nums1, nums2, l1, l2, i, heigh)
 	}
-	return findMedian(nums1, nums2, l1, l2, k+1, heigh)
+	maxFirstHalf = float64(nums1[i-1])
+	if nums1[i-1] < nums2[j-1] {
+		maxFirstHalf = float64(nums2[j-1])
+	}
+	if !isEven {
+		return maxFirstHalf
+	}
+	minSecondHalf = float64(nums1[i])
+	if nums1[i] > nums2[j] {
+		minSecondHalf = float64(nums2[j])
+	}
+
+	return (maxFirstHalf + minSecondHalf) / 2
 }
 
-/* thought process
-assume nums1[k] is the median
-=>
-in nums1, k elements <= nums1[k], l1-k elements >= nums1[k]
-in nums2, (l1+l2)/2 - k elements <= nums1[k], (l2-l1)/2 +k elements >= nums1[k]
-=>
-exist k such that nums2[(l1+l2)/2 - k - 1] <= nums1[k] <= nums2[(l1+l2)/2 - k]
- (l1+l2)/2 -k < l2 => (l1- l2) < 2k
- (l1+l2)/2 -k >0 => (l1+l2) > 2k
-**/
+// find the partition i, j where nums1[i-1] <= nums2[j] && nums2[j-1] <= num1[i]
--- 0004.median-two-sorted-array/median_two_sorted_array_test.go
+++ 0004.median-two-sorted-array/median_two_sorted_array_test.go
@@ -30,5 +30,10 @@ func TestFindMedianSortedArrays(t *testing.T) {
 	nums1 = []int{1, 2, 4}
 	nums2 = []int{3, 5, 6}
 	result = findMedianSortedArrays(nums1, nums2)
-	assert.Equal(t, 3.0, result)
+	assert.Equal(t, 3.5, result)
+
+	nums1 = []int{1, 2, 3}
+	nums2 = []int{4, 5, 6}
+	result = findMedianSortedArrays(nums1, nums2)
+	assert.Equal(t, 3.5, result)
 }
